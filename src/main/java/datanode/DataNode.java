fd53d72bce0fb637b39aae72eded375597acdeef
Saurabh <saurabh.k.1290@gmail.com>
2018-10-15 13:38:34 -0500

    data node data structure added.

--- src/main/java/datanode/DataNode.java
+++ src/main/java/datanode/DataNode.java
@@ -1,4 +1,10 @@
 package datanode;
 
+import java.util.Map;
+import java.util.Set;
+
 public class DataNode implements IDataNode {
+
+    public Map<Integer, Set<Integer>> storeMap; // HashIndex --> Set(replica ID Set)
+
 }
