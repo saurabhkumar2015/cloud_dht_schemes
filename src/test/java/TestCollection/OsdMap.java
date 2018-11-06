2ccf8c929ac48f2ea125456c2047875e17779e0f
jaivardhan1209 <jaivardhan1209@gmail.com>
2018-11-06 11:24:34 -0600

    Update OsdMap.java
    Added the logic to move files from cluster on addition of new node

--- src/test/java/TestCollection/OsdMap.java
+++ src/test/java/TestCollection/OsdMap.java
@@ -1,55 +1,143 @@
 package TestCollection;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
+import java.util.Random;
+import javafx.util.Pair;
 
 public class OsdMap {
 	
 	private static OsdMap single_instance = null;
 	
-	public OsdNode root;
+	public OsdNode root= null;
 	
-	public int maxclusterInlevel = 2;   // This value will come from Configuration file
+	public int maxclusterInlevel;   // This value will come from Configuration file
+	
+	public int depthofOsdMap;  // This is the calculated value using #node & #clusterSize
 	
 	public Queue<OsdNode> Parentqueue = new LinkedList<>();
 	
+	public HashGenerator hashGenerator;
+	
+	private OsdMap(int maxclusterInaNode, int depth)
+	{
+		this.maxclusterInlevel = maxclusterInaNode;
+		this.depthofOsdMap = depth;
+		hashGenerator = HashGenerator.getInstance();
+	}
 	
 	
 	// we can pass the configuration here
-	public static OsdMap getInstance() 
+	public static OsdMap getInstance(int maxclusterInaNode, int depth) 
     { 
         if (single_instance == null) 
-            single_instance = new OsdMap(); 
+            single_instance = new OsdMap(maxclusterInaNode, depth); 
   
         return single_instance; 
     } 
 	
+	// Utility funtion to generate weight
+	public double randomWeightGenerator()
+	{
+		Random rand = new Random();
+		return rand.nextDouble();
+	}
+	
 	// Input to populate the osd map need to think
-	public void AddNodeToOsdMap(double weight, int clusterId, int nodeId)
+    public void AddNodeToOsdMap(int clusterId, int nodeId)
 	{
 		if(root == null)
 		{
 			OsdNode currentnode = new OsdNode();
-			currentnode.AddNode(weight, clusterId, nodeId);
+			if(depthofOsdMap == 1)
+			currentnode.AddNode(randomWeightGenerator(), clusterId, nodeId, 1);
+			else
+				currentnode.AddNode(0, clusterId, -1, 1);
 			
 			// set root of the Osd map
 			root = currentnode;
 		}
 	if(this.root.clusterCountInLevel < maxclusterInlevel)
 	{
-		this.root.AddNode(weight, clusterId, nodeId);
+		if(depthofOsdMap == 1)
+		   this.root.AddNode(randomWeightGenerator(), clusterId, nodeId,1);
+		else
+			this.root.AddNode(0, clusterId, -1,1);
 	}
 	else
 	{
-			OsdNode leftNode = findTheClusterNodetoAddIterative(root.headNode.leftNode, root.headNode, 1);
-			if(leftNode != null)
+			Pair<OsdNode, Integer> leftNodePair = findTheClusterNodetoAddIterative(root.headNode.leftNode, root.headNode, 1);
+			if(leftNodePair != null)
 			{
-				leftNode.AddNode(weight, clusterId, nodeId);
+				Integer value = leftNodePair.getValue();
+				OsdNode key = leftNodePair.getKey();
+				if(value == depthofOsdMap)
+				{
+					key.AddNode(randomWeightGenerator(), clusterId, nodeId,value);
+				}
+				else
+					key.AddNode(0, clusterId, -1,value);				
 			}
 	}
 	}
 	
-   public void ShowOsdMap(OsdNode currentNode, int level)
+    // Add extra node after OSD Map Creation
+    public void AddExtraNodeToOsdMap(int clusterId, int nodeId)
+    {
+    	Pair<OsdNode, Integer> leftNodePair = findTheClusterNodetoAddIterative(root.headNode.leftNode, root.headNode, 1);
+		if(leftNodePair != null)
+		{
+			Integer value = leftNodePair.getValue();
+			OsdNode key = leftNodePair.getKey();
+			if(value == depthofOsdMap)
+			{
+				Node newlyAddedNode = key.AddNode(randomWeightGenerator(), clusterId, nodeId,value);
+				// Need to move the files from other node in sub cluster
+				MoveFileInClusterOnNewNodeAddition(key, newlyAddedNode);	
+			}
+			else
+				key.AddNode(0, clusterId, -1,value);				
+		}
+    }
+    
+    public void MoveFileInClusterOnNewNodeAddition(OsdNode headofCluster, Node newlyAddedNode)
+    {
+    	// Iterate through all the node except the newlyAdded node and move files from other node to new node.
+    	double weightofSubCluster = sumofClusterNode(headofCluster);
+    	Node tempNode = headofCluster.headNode;
+    	while(tempNode.nodeId != newlyAddedNode.nodeId)
+    	{
+    		// Get all files of this node 
+    		ArrayList<DataObject> filesForNode = CephDataNode.getInstance().map.get(tempNode.nodeId);
+    		
+    		// Iterate over files to check wheather to move or not
+    		if(filesForNode != null)
+    		{
+    			for(DataObject obj : filesForNode)
+    			{
+    				double hashvalue = HashGenerator.getInstance().generateHashValue(newlyAddedNode.clusterId, obj.placementGroup,obj.replicaId, weightofSubCluster);
+    				if(hashvalue < (newlyAddedNode.weight / weightofSubCluster))
+    				{
+    					// file will move from temp node to newly added node
+    					System.out.println("file with fileName " + obj.fileName + " with replication " + obj.replicaId + " moves from node " + tempNode.nodeId + " to node " + newlyAddedNode.nodeId);
+                        // TODO : Add the file to local system of datanode and remove from source node
+    					CephDataNode.getInstance().addDataToNode(newlyAddedNode.nodeId, obj.fileName, obj.placementGroup, obj.replicaId);
+    					// remove the file from the source 
+    					// TODO: Not required to write code here.
+    				}
+    			}
+    			
+    		}
+    		tempNode = tempNode.nextNode;
+    		weightofSubCluster = weightofSubCluster - tempNode.weight;
+    	}
+    	
+    }
+    
+    // Traverse the OSD map and show the snapshot 
+   public void ShowOsdMap(OsdNode currentNode)
    {
 	   //System.out.println("Show the nodes of osd map");
 	   
@@ -63,26 +151,108 @@ public class OsdMap {
 		   return;
 	   }
 	   // show the current node 
-	   currentNode.ShowCurrentNode(currentNode, level);
+	   currentNode.ShowCurrentNode(currentNode);
 	   // iterate over the next node and show their child
 	   Node tempNode = currentNode.headNode;
 	   while(tempNode != null)
 	   {
-	     ShowOsdMap(tempNode.leftNode, level + 1);
+	     ShowOsdMap(tempNode.leftNode);
 	     tempNode = tempNode.nextNode;
 	   }
    }
    
+   // Find the given nodeId in the OsdMap
    public void FindNodeInOsdMap(int nodeId)
    {
 	   _findNodeInOsdMap(root,nodeId, 1, true);
    }
    
+   // Set the activation flag false for given nodeId
    public void DeleteNode(int nodeId)
    {
 	   _findNodeInOsdMap(root,nodeId, 1, false);
    }
    
+   // Once we have weight distrubuted for leaf node, then populate weight from leaf to root
+   public void PopulateWeightOfInternalNode(OsdNode node)
+   {
+	   _populateWeight(root);
+   }
+  
+   // Find the node containing given file with replication value
+   // Ceph Algorithm for Finding NodeId for given filename and replica
+   public int findNodeWithRequestedReplica(int replicaId, int placementGroupId)
+   {
+	 //System.out.println("The placement Group for file: " +  fileName + " is : " + placementGroupId + " with replicaId: " + replicaId);
+	 int nodeId =  _findNodeWithRequestedReplica(root, placementGroupId, replicaId, 1);
+	 return nodeId;
+   }
+   
+   
+   public void AddFileToCephSystem(String fileName, int replicaId, int placementGroupSize)
+   {
+	   int placementGroupId = hashGenerator.getPlacementGroupIdFromFileName(fileName, placementGroupSize);
+	   int nodeId = findNodeWithRequestedReplica(replicaId, placementGroupId); 
+
+	   // we can add files to CephDataNode
+	    CephDataNode.getInstance().addDataToNode(nodeId, fileName, placementGroupId, replicaId);
+   }
+   private int _findNodeWithRequestedReplica(OsdNode headNode, int placementGroupId, int replicaId, int level)
+   {
+	   double totalclusterSum = sumofClusterNode(headNode);
+	   
+	   // Now find the hash value of each node in cluster and follow the path
+	   // where the hashvalue is less than node weight / Total cluster weight
+	   
+	   Node tempNode = headNode.headNode;
+	   double subClusterSum = totalclusterSum;
+	   while(tempNode != null)
+	   {
+		   double hashval = hashGenerator.generateHashValue(tempNode.clusterId, placementGroupId, replicaId, subClusterSum);
+		   if(hashval < (tempNode.weight / subClusterSum))
+			   break;
+		   subClusterSum = subClusterSum - tempNode.weight;
+		   tempNode = tempNode.nextNode;
+	   }
+	   
+	   if(tempNode != null && level < depthofOsdMap)
+		   return _findNodeWithRequestedReplica(tempNode.leftNode,placementGroupId, replicaId, level + 1);
+	   if(tempNode != null && level == depthofOsdMap)
+		   return tempNode.nodeId;
+	   else
+		   return -2;
+   }
+   
+   private double _populateWeight(OsdNode node)
+   {
+	   if(node != null && node.headNode.level == depthofOsdMap)
+	   {
+		   return sumofClusterNode(node);
+	   }
+	   if(node == null)
+		   return 0;
+	   Node tempNode = node.headNode;
+	   double sum = 0;
+	   while(tempNode != null)
+	   {
+		   tempNode.weight = _populateWeight(tempNode.leftNode);
+		   sum += tempNode.weight;
+		   tempNode = tempNode.nextNode;
+	   }
+	   return sum;
+   }
+   
+   private double sumofClusterNode(OsdNode node)
+   {
+	   Node temp = node.headNode;
+	   double sum = 0;
+	   while(temp != null)
+	   {
+		   sum = sum + temp.weight;
+		   temp = temp.nextNode;
+	   }
+	   return sum;
+   }
    private void _findNodeInOsdMap(OsdNode node, int nodeId, int level, boolean isActive)
    {
 	   if(node == null)
@@ -124,7 +294,7 @@ public class OsdMap {
    }
    
    // Private member function
-  private OsdNode findTheClusterNodetoAddIterative(OsdNode currentnode, Node parentnode, int level)
+   private Pair<OsdNode, Integer> findTheClusterNodetoAddIterative(OsdNode currentnode, Node parentnode, int level)
    {
    
 	   if(currentnode == null)
@@ -135,13 +305,11 @@ public class OsdMap {
 			   
 			   // Add this head node to next level parent queue
 			   Parentqueue.add(newlyAddedNode);
-			   
-		   }
-		   return currentnode;
+		   return new Pair<>(newlyAddedNode, parentnode.level +1);
 		}
-	   
+	   }
 	   if(currentnode.clusterCountInLevel < maxclusterInlevel)
-		   return currentnode;
+		   return new Pair<>(currentnode, parentnode.level+1);
 	   
 	   // if above condition not satisfied then we need to move to next node of the parent level
 	   Node nextParent = parentnode.nextNode;
@@ -154,10 +322,10 @@ public class OsdMap {
 			   OsdNode newlyAddedNode = new OsdNode();
 			   nextParent.leftNode = newlyAddedNode;
 			   Parentqueue.add(newlyAddedNode);
-			   return newlyAddedNode;
+			   return new Pair<>(newlyAddedNode, nextParent.level + 1);
 		   }
 		   else if(nextChildNode.clusterCountInLevel < maxclusterInlevel)
-			   return nextChildNode;
+			   return new Pair<>(nextChildNode, nextParent.level + 1);
 		   else
 		   {
 			   nextParent = nextParent.nextNode;
@@ -173,10 +341,10 @@ public class OsdMap {
 	    	   OsdNode newlyAddedNode = new OsdNode();
 	    	   nextlevelParent.headNode.leftNode = newlyAddedNode;
 	    	   Parentqueue.add(newlyAddedNode);
-	    	   return newlyAddedNode;
+	    	   return new Pair<>(newlyAddedNode, nextlevelParent.headNode.level + 1);
 	     }
 	     else if(nextlevelParent.headNode.leftNode.clusterCountInLevel < maxclusterInlevel)
-			   return nextlevelParent.headNode.leftNode;
+			   return new Pair<>(nextlevelParent.headNode.leftNode, nextlevelParent.headNode.level + 1);
 	     else
 	     {
 	    	 Node nextNodeInLevel = nextlevelParent.headNode.nextNode;
@@ -188,10 +356,10 @@ public class OsdMap {
 	    	    	   OsdNode newlyAddedNode = new OsdNode();
 	    	    	   nextNodeInLevel.leftNode = newlyAddedNode;
 	    	    	   Parentqueue.add(newlyAddedNode);
-	    	    	   return newlyAddedNode;
+	    	    	   return new Pair<>(newlyAddedNode, nextNodeInLevel.level + 1);
 	    	     }
 	    	     else if(nextNodeInLevel.leftNode.clusterCountInLevel < maxclusterInlevel)
-	    			   return nextNodeInLevel.leftNode;
+	    			   return new Pair<>(nextNodeInLevel.leftNode, nextNodeInLevel.level + 1);
 	    	     else
 	    	    	 nextNodeInLevel = nextNodeInLevel.nextNode;
 	    	 }
@@ -201,5 +369,4 @@ public class OsdMap {
 	   return null;
 	   
    }
-
-}
+   }
