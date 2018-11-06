32e707d4a717603f3c6b44ec93687ae0498d872e
jaivardhan1209 <jaivardhan1209@gmail.com>
2018-11-06 11:30:30 -0600

    Create CephDataNode.java
    This is instance of Data Node . In real system this code will go to Data Node.

---
+++ src/test/java/TestCollection/CephDataNode.java
@@ -0,0 +1,63 @@
+package TestCollection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CephDataNode {
+    public Map<Integer, ArrayList<DataObject>> map = new HashMap<Integer, ArrayList<DataObject>>();
+    
+    private static CephDataNode single_instance = null;
+   // we can pass the configuration here
+ 	public static CephDataNode getInstance() 
+     { 
+         if (single_instance == null) 
+             single_instance = new CephDataNode(); 
+   
+         return single_instance; 
+     } 
+ 	
+    
+    public void addDataToNode(int nodeId, String fileName, int placementGroup, int replicaId)
+    {
+    	// Create the Data Node 
+    	DataObject newlyAddedfile = new DataObject(fileName, placementGroup, replicaId);
+    	// If node is already there then append the Data object to the Node List
+    	ArrayList<DataObject> value = map.get(nodeId);
+    	if (value != null) {
+    	    value.add(newlyAddedfile);	
+    	} else {
+    	    // No such key exists
+    		map.put(nodeId, new ArrayList<DataObject>() {{add(newlyAddedfile);}});
+    	}
+    }
+    
+    public void ShowNodeContainer()
+    {
+    	for (Map.Entry<Integer,ArrayList<DataObject>> entry : map.entrySet())
+    	{
+    		System.out.println("Node : -> " + entry.getKey());
+    		ArrayList<DataObject> fileObject = entry.getValue();
+    		for( DataObject dobj : fileObject)
+    		{
+    			System.out.println("fileName : " + dobj.fileName + " PlacementGroup : " + dobj.placementGroup + " ReplicaId : " + dobj.replicaId);
+    		}
+    	}
+    }
+    
+    public void ShowNodeContainer(int nodeId)
+    {
+    	ArrayList<DataObject> fileObject = map.get(nodeId);
+    	if(fileObject != null)
+    	{
+    	for( DataObject dobj : fileObject)
+    	{
+    		System.out.println("fileName : " + dobj.fileName + " PlacementGroup : " + dobj.placementGroup + " ReplicaId : " + dobj.replicaId);
+    	}
+    	}    	
+    }
+    
+}
+
+
