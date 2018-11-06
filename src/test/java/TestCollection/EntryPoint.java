d8371cc08367edc4df85ce328b08776f1d8c9b9b
jaivardhan1209 <jaivardhan1209@gmail.com>
2018-11-06 11:25:21 -0600

    Update EntryPoint.java
    Modified the entry point to test OsdMap

--- src/test/java/TestCollection/EntryPoint.java
+++ src/test/java/TestCollection/EntryPoint.java
@@ -1,6 +1,7 @@
 package TestCollection;
 
 import java.util.Random;
+import config.DHTConfig;
 
 public class EntryPoint {
 
@@ -8,115 +9,81 @@ public class EntryPoint {
 		// TODO Auto-generated method stub
      System.out.println("OsdMap status");
      
-     
+     // BootStrapping
+     // Step 1 : using no of nodes and cluster size from configuration find the depth of Osd Map
+     DHTConfig config = new DHTConfig();
+     int depth = FindDepthOfOsdMap(config.numNodeIds, config.cephMaxClusterSize);
+     System.out.println("Osd Depth is :" + depth);
+     // Step 2 : create the Osd Map with Leaf node with weight 
      // Populate the osd map from Configuration
-     OsdMap mapInstance = OsdMap.getInstance();
+      OsdMap mapInstance = OsdMap.getInstance(config.cephMaxClusterSize,depth);
      
-     // Populate OsdMap
-     PopulateOsdMap(mapInstance);
+      // 
+     // Step 3: populate the internal node weight using the commutative weight of child node
+     // step 3.a Populate OsdMap
+      PopulateOsdMap(mapInstance, config.numNodeIds, depth,config.cephMaxClusterSize );
+      
+     //set the internal node weight
+      mapInstance.PopulateWeightOfInternalNode(mapInstance.root);
+     // Till this Point Osd Map is populated and build properly.
+     // Find the node containing PG = 5 & replication 2 I can change the name to add file : TODO : Refactor this code
+      AddFilesToCephSystem();
+      
+     //CephDataNode.getInstance().ShowNodeContainer();
      
+     // Add an extra node to ceph osd system 
+      mapInstance.AddExtraNodeToOsdMap(15, 95);
      // Show OsdMap status
-     OsdMap.getInstance().ShowOsdMap(mapInstance.root,1);
-    
-     // Add extra node to Osd Map
-     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 75);
-     
-     // Check the added node 75 in OsdMap
-     mapInstance.FindNodeInOsdMap(68);
-     
-     // Mark a node failure
-     mapInstance.DeleteNode(73);
+    // mapInstance.ShowOsdMap(mapInstance.root);
+     // CephDataNode.getInstance().ShowNodeContainer(80);
      
 	}
 	
-	public static void PopulateOsdMap(OsdMap mapInstance)
+	public static void AddFilesToCephSystem()
 	{
-		 mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 1, 2);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 2, 3);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 4);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 5);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 6);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 7);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 8);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 9);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 10);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 11);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 12);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 13);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 14);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 15);
-	     
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 1, 16);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 2, 17);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 18);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 19);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 20);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 21);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 22);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 23);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 24);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 25);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 26);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 27);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 28);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 29);
-	     
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 30);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 31);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 32);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 33);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 34);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 35);
-	      
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 36);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 37);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 38);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 39);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 40);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 41);
-	     
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 42);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 43);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 44);
-	     
-	     
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 1, 46);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 2, 47);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 48);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 49);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 50);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 51);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 52);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 53);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 54);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 55);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 56);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 57);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 58);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 59);
-	     
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 60);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 61);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 3, 62);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 4, 63);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 64);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 65);
-	      
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 66);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 67);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 68);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 69);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 70);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 71);
-	     
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 72);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 73);
-	     mapInstance.AddNodeToOsdMap(randomWeightGenerator(), 5, 74);
+		DHTConfig config = new DHTConfig();
+		int depth = FindDepthOfOsdMap(config.numNodeIds, config.cephMaxClusterSize);
+		OsdMap mapInstance = OsdMap.getInstance(config.cephMaxClusterSize,depth);
+		
+		for(int i = 0; i < 1000; i++)
+		{
+			Random r = new Random();
+			int replicaId = r.nextInt(3) + 1;
+			String fileName = "CloudComputing" + i;
+			mapInstance.AddFileToCephSystem(fileName, replicaId, config.PlacementGroupMaxLimit);
+		}
+		
+	}
+	
+	public static void PopulateOsdMap(OsdMap mapInstance, int nodeIds, int depth, int clusterSize)
+	{
+		for(int i = 1; i < depth;i++)
+		{
+			// populate internal node
+			for(int j = 0; j< Math.pow(clusterSize,i); j++)
+			{
+				mapInstance.AddNodeToOsdMap(randomClusterNoGenerator(), 1);
+			}
+		}
+		// Insert regular data node
+		for(int k = 2; k<= nodeIds; k++)
+		{
+			mapInstance.AddNodeToOsdMap(randomClusterNoGenerator(), k);
+		}
 	}
 
-	public static double randomWeightGenerator()
+	public static int FindDepthOfOsdMap(int countOfNodes, int nodePerCluster)
 	{
-		Random rand = new Random();
-		return rand.nextDouble();
+		// depth should be log base nodeperCluster (countOfNodes) ceiling value
+		return (int) Math.ceil((Math.log(countOfNodes) / Math.log(nodePerCluster)));
+	}
+	
+	public static int randomClusterNoGenerator()
+	{
+		Random r = new Random();
+		int low = 1;
+		int high = 21;
+		int result = r.nextInt(high-low) + low;
+		return result;
 	}
 }
