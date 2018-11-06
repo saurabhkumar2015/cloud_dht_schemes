3cc830c91951f847c254d5924ab3fb501c3d930e
jaivardhan1209 <jaivardhan1209@gmail.com>
2018-11-06 11:31:36 -0600

    Create DataObject.java
    Object for keeping PlacementGroup, Replica for each Data Node.

---
+++ src/test/java/TestCollection/DataObject.java
@@ -0,0 +1,17 @@
+package TestCollection;
+
+public class DataObject {
+
+	public String fileName;
+	
+	public int placementGroup;
+	
+	public int replicaId;
+	
+	public DataObject(String fileName, int placementGroup, int replicaId)
+	{
+		this.fileName = fileName;
+		this.placementGroup = placementGroup;
+		this.replicaId = replicaId;
+	}
+}
