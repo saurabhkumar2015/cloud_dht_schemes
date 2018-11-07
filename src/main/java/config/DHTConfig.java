b91a3cf62b6e7fd8185951b4eea67aea6ddfd844
jaivardhan1209 <jaivardhan1209@gmail.com>
2018-11-07 13:38:29 -0600

    Update DHTConfig.java
    Added the config file for ceph.

--- src/main/java/config/DHTConfig.java
+++ src/main/java/config/DHTConfig.java
@@ -10,13 +10,16 @@ import java.util.Map;
 public class DHTConfig {
 
     public long version;
-    public int numNodeIds;
+    public int numNodeIds = 74;
     public String proxyId;
     public boolean verbose;
     public String dhtType; // centralized or distributed
     public String scheme; // ring, elastic or ceph
     public byte replicationFactor;
-    public byte cephMaxClusterSize;
+    public byte cephMaxClusterSize = 5;
     public String nodeMapLocation;
     public Map<Integer, String> nodesMap; // Node Id --> Ip:port for a datanode
+    
+    // Added for ceph 
+    public int PlacementGroupMaxLimit = 50;
 }
