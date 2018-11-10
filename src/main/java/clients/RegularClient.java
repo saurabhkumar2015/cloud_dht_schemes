7e85f774e9cfb58605577db8618736becef6ee22
Saurabh <saurabh.k.1290@gmail.com>
2018-11-10 11:45:38 -0600

    Regular client refactor.

--- src/main/java/clients/RegularClient.java
+++ src/main/java/clients/RegularClient.java
@@ -1,15 +1,8 @@
 package clients;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import config.ConfigLoader;
 import config.DHTConfig;
-import schemes.IDHTScheme;
-import sun.security.krb5.Config;
-
-import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.Map;
 
 public class RegularClient {
 
@@ -19,16 +12,14 @@ public class RegularClient {
 
     public static void main(String[] args) throws Exception {
 
-        if(args.length != 2) {
-         throw new Exception("Please specify Two arguments. \n 1) Config file absolute path \n 2) File containing list of files to write in DHT.");
-        }
+        if(args.length != 2) throw new Exception("Please specify Two arguments. \n 1) Config file absolute path \n 2) File containing list of files to write in DHT.");
+
         ConfigLoader.init(args[0]);
         DHTConfig config = ConfigLoader.config;
         initRegularClient(config);
         FileReader fileList = new FileReader(args[1]);
 
     }
-
 
     private static void initRegularClient(DHTConfig config) {
         scheme = config.scheme;
