5acf2e953fe380ce5330031c14d2b7797ef60eda
jaivardhan1209 <jaivardhan1209@gmail.com>
2018-11-11 12:11:07 -0600

    Update DataObject.java

--- src/main/java/ceph/DataObject.java
+++ src/main/java/ceph/DataObject.java
@@ -1,17 +1,18 @@
 package ceph;
 
 public class DataObject {
-
-	public String fileName;
 	
 	public int placementGroup;
 	
 	public int replicaId;
 	
-	public DataObject(String fileName, int placementGroup, int replicaId)
+	public String fileName;
+	
+	public DataObject(int placementGroup, int replicaId, String fileName)
 	{
-		this.fileName = fileName;
 		this.placementGroup = placementGroup;
 		this.replicaId = replicaId;
+		this.fileName = fileName;
 	}
 }
+
