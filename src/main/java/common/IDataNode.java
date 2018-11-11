892283fe502c088f0e9b3db61a7633368ddb0f39
Saurabh <saurabh.k.1290@gmail.com>
2018-11-11 10:56:24 -0600

    commit

--- src/main/java/common/IDataNode.java
+++ src/main/java/common/IDataNode.java
@@ -2,7 +2,15 @@ package common;
 
 public interface IDataNode {
 
+	/**
+	 * modify IRoutingTable to include the new node
+	 * call writeFile on Data Nodes
+	 * call deleteFile on DataNodes
+	 * @param fileName
+	 * @param replicaId
+	 */
 	public void writeFile(String fileName, String replicaId);
+
 
 	public void deleteFile(String fileName);
 
