2584d6a1135dd99f13b91d774bd728aeb49f5a4e
Kavitha Rajendran <kavitha.rajendran88@gmail.com>
2018-11-11 16:31:05 -0600

    Updating return type

--- src/main/java/common/IRoutingTable.java
+++ src/main/java/common/IRoutingTable.java
@@ -3,8 +3,8 @@ package common;
 public interface IRoutingTable {
 
     int getNodeId(String fileName, int replicaId);
-    void addNode(int nodeId);
-    void deleteNode(int nodeId);
-    void loadBalance(int nodeId, double loadFactor);
+    IRoutingTable addNode(int nodeId);
+    IRoutingTable deleteNode(int nodeId);
+    IRoutingTable loadBalance(int nodeId, double loadFactor);
 
 }
