cf20d3dcd6a11d9ce4fd4f1e79084fa48e63d78e
tulikaMithal <tm290392@gmail.com>
2018-11-18 23:04:15 -0600

    Added epoch sync

--- src/main/java/proxy/ProxyServer.java
+++ src/main/java/proxy/ProxyServer.java
@@ -5,11 +5,13 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import common.CephPayload;
 import common.Constants;
+import common.EpochPayload;
 import common.IRoutingTable;
 import common.LoadBalance;
 
@@ -30,6 +32,7 @@ import sun.misc.IOUtils;
 public class ProxyServer {
 	
 	private static String scheme;
+	private static DHTConfig config;
 	private static CephRoutingTable ceph_routing_table;
 	private static RingRoutingTable ring_routing_table;
 	private static ElasticRoutingTable elastic_routing_table;
@@ -41,7 +44,7 @@ public class ProxyServer {
     	
     	//get initial Instances of routing tables or osdMap 
     	scheme = config.scheme;
-
+    	
         switch (scheme) {
             case "RING":
             case "ring":
@@ -64,34 +67,27 @@ public class ProxyServer {
     }
     
     
-    /* Sending updated DHT to datanodes */
-    
-    public static void sendUpdatedDhtToDatanodes(DHTConfig config) {
-    	
-    	Map<Integer,String> dataNodes = config.nodesMap;
-    	
-    	if(scheme.equals("CEPH") || scheme.equals("ceph")) {
-    		
-    		 for (Entry<Integer, String> entry : dataNodes.entrySet())  {
-    			 sendMsg.sendMessage(entry.getValue(), Constants.NEW_VERSION, ceph_routing_table);
-    		 }
-    	          
-    	}
-    	
-    	else if(scheme.equals("RING") || scheme.equals("ring")) {
-    		for (Entry<Integer, String> entry : dataNodes.entrySet())  {
-   			 sendMsg.sendMessage(entry.getValue(),Constants.NEW_VERSION, ring_routing_table);
-   		 	}
-    	}
-    	
-    	else{
-    		for (Entry<Integer, String> entry : dataNodes.entrySet())  {
-   			 sendMsg.sendMessage(entry.getValue(), Constants.NEW_VERSION, elastic_routing_table);
-   		 	}
-    	}
-    	
+    public static void sendUpdatedDHT() throws Exception {
+    	switch (scheme) {
+        case "RING":
+        case "ring":
+            break;
+        case "ELASTIC":
+        case "elastic":
+            break;
+        case "CEPH":
+        case "ceph":
+            List<Integer> liveNodes = ceph_routing_table.getLiveNodes();
+            for(int id: liveNodes) {
+            	System.out.println("Live node "+id);
+            	EpochPayload payload = new EpochPayload("true", ceph_routing_table);
+            	sendMsg.sendMessage(config.nodesMap.get(id), Constants.NEW_VERSION, payload);
+            }
+            break;
+        default:
+            throw new Exception("Incompatible DHT schema found!");
+    	}    
     }
-    
     
     public static void main(String[] argv) throws Exception {
 		
@@ -100,7 +96,7 @@ public class ProxyServer {
     	
     	/*loading config file*/
         ConfigLoader.init(argv[0]);
-        DHTConfig config = ConfigLoader.config;
+        config = ConfigLoader.config;
         initProxy(config);
     	
     	 
@@ -110,106 +106,99 @@ public class ProxyServer {
 	     
 	     try {
 	    	 
-		server = new ServerSocket(5000);
-	    	while(true) {
+		 server = new ServerSocket(5000);
+	     while(true) {
 	    		
-	    		// listening on port 5000
-	            // takes input from the client socket 
-			   /*  in = socket.getInputStream(); 
-			    byte[] bytes = IOUtils.readFully(in, -1, true);
-			        
-			     Request message = SerializationUtils.deserialize(bytes);*/
+	    	
 				socket = server.accept();
 				ObjectInputStream ins = new ObjectInputStream(socket.getInputStream());
 		        Request message = (Request) ins.readObject();
-
-		    	if((message.getType()).equals(Constants.ADD_NODE)) {
-		    		
-		    		Integer nodeId = (Integer) message.getPayload();
-                    System.out.println("Add node " + nodeId);
-                    CephRoutingTable updated_ceph_routing_table = (CephRoutingTable)ceph_routing_table.addNode(nodeId);
-                    ceph_routing_table = updated_ceph_routing_table;
-                    
-                    Node headNode = ceph_routing_table.mapInstance.FindNodeInOsdMap(nodeId);
-                    
-	                   Node temp = headNode;
-	                   Node temp1 = headNode.nextNode;
-	             	   double sum = 0;
-	             	   while(temp != null)
-	             	   {
-	             		   sum = sum + temp.weight;
-	             		   temp = temp.nextNode;
-	             	   }
-	             	   
-	             	  String newNodeStr = config.nodesMap.get(headNode.nodeId);
-            		  int newNodeClusterId = headNode.clusterId;
-            		  double newNodeWt = headNode.weight;
-            		  
-	             	   while(temp1 != null) {
-	             		   
-	             		   double weight = temp1.weight;
-	             		   String nodeIp = config.nodesMap.get(temp1.nodeId);
-	             		   CephPayload payload = new CephPayload(newNodeStr, newNodeClusterId, newNodeWt, sum, false, ceph_routing_table);
-	             		   System.out.println("Move file called::" + payload + " to node IP: " + nodeIp);
-	             		   sendMsg.sendMessage(nodeIp, Constants.MOVE_FILE, payload);
-	             		   sum = sum - weight;
-	             		   temp1 = temp1.nextNode;
-	             	   }
-                       
-                 
-		    		
-		    	}
+		        
+		        if(scheme.toUpperCase().equals("CEPH")) {
+		        	
+			    	if((message.getType()).equals(Constants.ADD_NODE)) {
+			    		
+			    		Integer nodeId = (Integer) message.getPayload();
+	                    System.out.println("Add node " + nodeId);
+	                    CephRoutingTable updated_ceph_routing_table = (CephRoutingTable)ceph_routing_table.addNode(nodeId);
+	                    ceph_routing_table = updated_ceph_routing_table;
+	                    
+	                    Node headNode = ceph_routing_table.mapInstance.FindNodeInOsdMap(nodeId);
+	                    
+		                   Node temp = headNode;
+		                   Node temp1 = headNode.nextNode;
+		             	   double sum = 0;
+		             	   while(temp != null)
+		             	   {
+		             		   sum = sum + temp.weight;
+		             		   temp = temp.nextNode;
+		             	   }
+		             	   
+		             	  String newNodeStr = config.nodesMap.get(headNode.nodeId);
+	            		  int newNodeClusterId = headNode.clusterId;
+	            		  double newNodeWt = headNode.weight;
+	            		  
+		             	   while(temp1 != null) {
+		             		   
+		             		   double weight = temp1.weight;
+		             		   String nodeIp = config.nodesMap.get(temp1.nodeId);
+		             		   CephPayload payload = new CephPayload(newNodeStr, newNodeClusterId, newNodeWt, sum, false, ceph_routing_table);
+		             		   System.out.println("Move file called::" + payload + " to node IP: " + nodeIp);
+		             		   sendMsg.sendMessage(nodeIp, Constants.MOVE_FILE, payload);
+		             		   sum = sum - weight;
+		             		   temp1 = temp1.nextNode;
+		             	   }
+		             	   
+		             	   sendUpdatedDHT();
+	                       
+	                   }
+			    	
+				    	if((message.getType()).equals(Constants.LOAD_BALANCE)) {
+				    		
+				    		LoadBalance lb = (LoadBalance) message.getPayload();
+				    		int nodeToBeBalanced = lb.nodeId;
+				    		double loadFactor = lb.loadFactor;
+				    		
+				    		System.out.println("DataNode to be loadbalanced "+nodeToBeBalanced);
+				    		System.out.println("Load Factor "+loadFactor);
+				    		
+				    		CephRoutingTable updated_ceph_routing_table = (CephRoutingTable)ceph_routing_table.loadBalance(nodeToBeBalanced, loadFactor);
+				    		
+		                    ceph_routing_table = updated_ceph_routing_table;
+		                    
+		                    Node headNode = ceph_routing_table.mapInstance.findHeadNodeOfTheCluster(nodeToBeBalanced);
+		                    
+			                Node temp = headNode;
+			     
+			             	   double sum = 0;
+			             	   while(temp != null)
+			             	   {
+			             		   sum = sum + temp.weight;
+			             		   temp = temp.nextNode;
+			             	   }
+			             	   
+			          
+		            		  
+		            	      Node ptr = headNode;
+		            	    
+			             	   while(ptr != null) {
+			             		   
+			             		   double weight = ptr.weight;
+			             		   int clusterId = ptr.clusterId;
+			             		   String nodeIp = config.nodesMap.get(ptr.nodeId);
+			             		   
+			             		   CephPayload payload = new CephPayload(nodeIp, clusterId, weight, sum, true, ceph_routing_table);
+			             		   System.out.println("Move file called::" + payload + " to node IP: " + nodeIp);
+			             		   sendMsg.sendMessage(nodeIp, Constants.MOVE_FILE, payload);
+			             		   sum = sum - weight;
+			             		   ptr = ptr.nextNode;
+			             	   }
+		                      
+			             	   sendUpdatedDHT();
+		                }
+		        	}	
 		    	
-		    	if((message.getType()).equals(Constants.LOAD_BALANCE)) {
-		    		
-		    		LoadBalance lb = (LoadBalance) message.getPayload();
-		    		int nodeToBeBalanced = lb.nodeId;
-		    		double loadFactor = lb.loadFactor;
-		    		
-		    		System.out.println("DataNode to be loadbalanced "+nodeToBeBalanced);
-		    		System.out.println("Load Factor "+loadFactor);
-		    		
-		    		CephRoutingTable updated_ceph_routing_table = (CephRoutingTable)ceph_routing_table.loadBalance(nodeToBeBalanced, loadFactor);
-		    		
-                    ceph_routing_table = updated_ceph_routing_table;
-                    
-                    Node headNode = ceph_routing_table.mapInstance.findHeadNodeOfTheCluster(nodeToBeBalanced);
-                    
-	                Node temp = headNode;
-	     
-	             	   double sum = 0;
-	             	   while(temp != null)
-	             	   {
-	             		   sum = sum + temp.weight;
-	             		   temp = temp.nextNode;
-	             	   }
-	             	   
-	          
-            		  
-            	      Node ptr = headNode;
-            	    
-	             	   while(ptr != null) {
-	             		   
-	             		   double weight = ptr.weight;
-	             		   int clusterId = ptr.clusterId;
-	             		   String nodeIp = config.nodesMap.get(ptr.nodeId);
-	             		   
-	             		   CephPayload payload = new CephPayload(nodeIp, clusterId, weight, sum, true, ceph_routing_table);
-	             		   System.out.println("Move file called::" + payload + " to node IP: " + nodeIp);
-	             		   sendMsg.sendMessage(nodeIp, Constants.MOVE_FILE, payload);
-	             		   sum = sum - weight;
-	             		   ptr = ptr.nextNode;
-	             	   }
-                       
-                 
-		    		
-		    	}
-		    	
-		   
-		    		
-		    			//compare epoch number & if greater...update dht and send messages to datanodes
-		    	
-		    	}
+		    }
 	    	
 		} catch (IOException e) { 
 			// TODO Auto-generated catch block
