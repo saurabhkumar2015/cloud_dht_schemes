c40244f38de1e676bff9c6156a34297c9f9d8361
Saurabh <saurabh.k.1290@gmail.com>
2018-11-20 16:55:12 -0600

    Gossip.

--- src/main/java/generators/GossipListGenerator.java
+++ src/main/java/generators/GossipListGenerator.java
@@ -1,4 +1,9 @@
 package generators;
 
 public class GossipListGenerator {
+    public static void main(String[] args) throws Exception {
+        if(args.length!=3) throw new Exception("Please specify Three arguments." +
+                "\n 1)Nodes.csv location"
+                + "\n 2) IP value");
+    }
 }
