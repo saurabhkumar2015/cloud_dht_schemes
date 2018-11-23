c63313041a5dabfb36af5fa30d127325e1994653
jaivardhan1209 <jaivardhan1209@gmail.com>
2018-11-23 14:49:46 -0600

    Update DataObject.java
    Added logic to compare two object.

--- src/main/java/ceph/DataObject.java
+++ src/main/java/ceph/DataObject.java
@@ -14,4 +14,20 @@ public class DataObject {
 		this.replicaId = replicaId;
 		this.fileName = fileName;
 	}
+	
+	@Override 
+	 public boolean equals (Object obj)
+	  {
+	   if (this==obj) return true;
+	   if (this == null) return false;
+	   if (this.getClass() != obj.getClass()) return false;
+	   // if pgroup and replica is equal.
+	   DataObject dbobj = (DataObject) obj ;
+	   return (this.placementGroup == dbobj.placementGroup && this.replicaId == dbobj.replicaId);
+	   }
+	
+	   @Override
+       public int hashCode() {
+           return 0;
+       }
 }
