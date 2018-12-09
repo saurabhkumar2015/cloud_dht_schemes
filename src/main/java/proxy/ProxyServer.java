8b08ee12bb9b1981c6313eb1dd393fea9527a955
Tulika Mithal <tm290392@gmail.com>
2018-12-09 07:33:27 -0600

    Update ProxyServer.java

--- src/main/java/proxy/ProxyServer.java
+++ src/main/java/proxy/ProxyServer.java
@@ -1,12 +1,16 @@
 package proxy;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import common.*;
 import ceph.CephRoutingTable;
@@ -25,7 +29,8 @@ public class ProxyServer {
 	private static String scheme;
 	private static DHTConfig config;
 	private static IRoutingTable routingTable;
-	private static IMessageSend sendMsg = new MessageSendImpl();
+	public static Map<Long,LogObject> map = new HashMap<Long,LogObject>();
+	
 	
 	/* Bootstrapping the DHT table according to scheme */
 	
@@ -56,29 +61,10 @@ public class ProxyServer {
 
     
     }
+   
     
-    
-    public static void sendUpdatedDHT(int nodeId, String type, double factor) throws Exception {
-    	
-            List<Integer> liveNodes = routingTable.giveLiveNodes();
-            
-            for(int id: liveNodes) {
-            	System.out.println("Live node "+id);
-            	
-            		  new Thread() {
-            		      public void run() {
-            		    	  
-            		    	    UpdateRoutingPayload payload = new UpdateRoutingPayload(nodeId, type, routingTable, factor);
-            	            	sendMsg.sendMessage(config.nodesMap.get(id), Constants.NEW_VERSION, payload);
-            		        
-            		      }
-            		  }.start();
-            	
-            }
-             
-    }
-    
-    public static void main(String[] argv) throws Exception {
+    @SuppressWarnings("resource")
+	public static void main(String[] argv) throws Exception {
 		
     	
     	if(argv.length != 1) throw new Exception("Please specify one arguments. \n 1) Config file absolute path \n");
@@ -88,66 +74,20 @@ public class ProxyServer {
         config = ConfigLoader.config;
         initProxy(config);
     	
-    	 
-		 Socket socket = null; 
-	     ServerSocket server  = null; 
-	     InputStream in =  null; 
+    	ServerSocket server  = null; 
+	    Socket socket=null;
+	    server = new ServerSocket(5000);
+	    
+	    while(true) {
+	        	try {
+		            socket = server.accept();
+		            ProxyWorker w = new ProxyWorker( routingTable,socket,config,new DataOutputStream(socket.getOutputStream()),new ObjectInputStream(socket.getInputStream()));
+		            w.run();
+	        	}catch(IOException e) {
+	        		socket.close();
+	        	}
+	        }
 	     
-	     try {
-	    	 
-		 server = new ServerSocket(5000);
-	     while(true) {
-	    		
-	    	
-				socket = server.accept();
-				ObjectInputStream ins = new ObjectInputStream(socket.getInputStream());
-		        Request message = (Request) ins.readObject();
-		        
-		        
-		        	
-			    	if((message.getType()).equals(Constants.ADD_NODE)) {
-			    		
-			    		Integer nodeId = (Integer) message.getPayload();
-	                    System.out.println("Add node " + nodeId);
-	                    IRoutingTable updated_routing_table =  routingTable.addNode(nodeId);
-	                    routingTable = updated_routing_table;
-	                    
-	                    sendUpdatedDHT(nodeId, Constants.ADD_NODE, 0);
-	                       
-	                 }
-			    	
-				    if((message.getType()).equals(Constants.LOAD_BALANCE)) {
-				    		
-				    		LoadBalance lb = (LoadBalance) message.getPayload();
-				    		int nodeToBeBalanced = lb.nodeId;
-				    		double loadFactor = lb.loadFactor;
-				    		
-				    		System.out.println("DataNode to be loadbalanced "+nodeToBeBalanced);
-				    		System.out.println("Load Factor "+loadFactor);
-				    		
-				    		IRoutingTable updated_routing_table =  routingTable.loadBalance(nodeToBeBalanced, loadFactor);
-		                    routingTable = updated_routing_table;
-		                
-			           	    sendUpdatedDHT(nodeToBeBalanced,Constants.LOAD_BALANCE, lb.loadFactor);
-		              }
-				    
-				    if((message.getType()).equals(Constants.DELETE_NODE)) {
-			    		
-			    		int nodeToBeDeleted = (Integer) message.getPayload();
-			    		
-			    		System.out.println("DataNode to be deleted "+nodeToBeDeleted);
-			    		
-			    		IRoutingTable updated_routing_table =  routingTable.deleteNode(nodeToBeDeleted);
-	                    routingTable = updated_routing_table;
-	                
-		           	    sendUpdatedDHT(nodeToBeDeleted, Constants.DELETE_NODE, 0);
-	               }
-		       }
-		      
-		} catch (IOException e) { 
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
 
     }
 
