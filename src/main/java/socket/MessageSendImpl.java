68e5b3187c5f8d85febc402d521f10510a8be1db
Tulika Mithal <tm290392@gmail.com>
2018-12-09 07:32:24 -0600

    Update MessageSendImpl.java

--- src/main/java/socket/MessageSendImpl.java
+++ src/main/java/socket/MessageSendImpl.java
@@ -7,11 +7,17 @@ import clients.RegularClient;
 import clients.RegularClientDemo;
 import common.Constants;
 import common.EpochPayload;
+import common.LogObject;
+import common.Payload;
+import common.UpdateRoutingPayload;
 import config.ConfigLoader;
+import proxy.ProxyServer;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -51,21 +57,62 @@ public class MessageSendImpl implements IMessageSend {
             oos.writeObject(request);
             stream = baos.toByteArray();
             out.write(stream);
-
-
+            
+            /*******Message sent at ************************************************************/
+            long start = System.nanoTime();
+            /***********************************************************************************/
+            
+            /******************************************For new version logs*********************/
+            if(type.equals(Constants.NEW_VERSION)) {
+            	input = new DataInputStream(socket.getInputStream());
+	            long end = System.nanoTime();
+	            ObjectInputStream ois = new ObjectInputStream(input);
+	            EpochPayload p = (EpochPayload) ois.readObject();
+	            LogObject obj = ProxyServer.map.get((p.newRoutingTable).getVersionNumber());
+	            	System.out.println("count "+obj.count+" livenodes "+obj.liveNodes);
+		            obj.count = obj.count+1;
+		            ProxyServer.map.put((p.newRoutingTable).getVersionNumber(),obj);
+		            
+		            if(obj.count == obj.liveNodes) {
+		            	System.out.println("count equalto live  "+obj.count+" livenodes "+obj.liveNodes);
+			            BufferedWriter writer = new BufferedWriter(new FileWriter(ConfigLoader.config.logFileForCC,true));
+			            writer.write(Long.toString(end-obj.start));
+			            writer.newLine();
+			            writer.close();
+			            ProxyServer.map.remove((p.newRoutingTable).getVersionNumber());
+		            }
+	            
+            }
+            
+            /*****************************************************************************************/
+            
+            /*******************************For write logs********************************************/
             if(type.equals(Constants.WRITE_FILE) || type.equals(Constants.ADD_FILES)) {
             	
 	            input = new DataInputStream(socket.getInputStream());
+	            
+	            long end = System.nanoTime();
+	            
 	            ObjectInputStream ois = new ObjectInputStream(input);
 	            EpochPayload p = (EpochPayload) ois.readObject();
 
 	            System.out.println("Received ack "+p.status);
-
-	            if((p.status).trim().equals("Fail due to version mismatch")){
+	            
+	            if(type.equals(Constants.WRITE_FILE)){
+		            BufferedWriter writer = new BufferedWriter(new FileWriter(ConfigLoader.config.logFileForWrite,true));
+		            writer.write(((Payload)payload).fileName+","+Long.toString((end-start))+","+p.status);
+		            writer.newLine();
+		            writer.close();
+            	}
+	           
+	            if((p.status).trim().equals("Fail due to version mismatch")) {
 	            		RegularClient.routingTable = p.newRoutingTable;
 		    		RegularClientDemo.routingTable = p.newRoutingTable;
-		    }
+	            }
             }
+            
+            /***************************************************************************************************/
+         
             else if(Arrays.asList(Constants.ADD_NODE, Constants.DELETE_NODE, Constants.LOAD_BALANCE ).contains(type)) {
                 if(ConfigLoader.config.dhtType.equalsIgnoreCase("distributed")) {
                     input = new DataInputStream(socket.getInputStream());
